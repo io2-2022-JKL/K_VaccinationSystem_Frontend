1c9a1b6137328c4cb52773c35133f6c91adb970e
witek0509x <32247322+witek0509x@users.noreply.github.com>
2022-04-11 00:33:18 +0200

    Feature/login logic (#14)
    * useLogin simple logic
    
    * protected sites, default login page

--- src/components/doctor/DoctorLoginPage.js
+++ src/components/doctor/DoctorLoginPage.js
@@ -1,10 +1,15 @@
 import {Link} from "react-router-dom";
+import useLogin from "../../logic/useLogin";
 
 export function DoctorLoginPage() {
+    const {LogIn} = useLogin();
+    const SubmitLogIn = () => {
+        LogIn("token", "doctor")
+    }
     return (
         <>
-            <h2> Strona logowania lekarza </h2>
-            <Link to="/doctor/dashboard">Logowanie</Link>
+            <h2> Strona logowania admina </h2>
+            <button onClick={SubmitLogIn}>Log In</button>
         </>
 
     )
