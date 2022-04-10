e25c874d94a0af24389be71489ada87f3639f836
witek0509x <witek.droch@gmail.com>
2022-04-11 00:45:39 +0200

    simple login page

--- src/components/patient/PatientLoginPage.js
+++ src/components/patient/PatientLoginPage.js
@@ -1,11 +1,25 @@
-import {Link} from "react-router-dom";
+import useLogin from "../../logic/useLogin";
+import "../../styles/loginPageStyles.css";
+import {useState} from "react";
 
 export function PatientLoginPage() {
+    const {LogIn} = useLogin();
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const SubmitLogIn = () => {
+        LogIn(password, "patient")
+    }
     return (
-        <>
-            <h2> Strona logowania pacjenta </h2>
-            <Link to="/patient/dashboard">Zaloguj</Link>
-        </>
+        <section className="form animated flipInX">
+            <h2>Zaloguj się</h2>
+            <form className="loginbox" autoComplete="off" onSubmit={SubmitLogIn}>
+                <input placeholder="Email" type="text" id="username" onChange={(e) => {setLogin(e.target.value)}}/>
+                <input placeholder="Hasło" type="password" id="password" onChange={(e) => {setPassword(e.target.value)}}/>
+                <button id="submit">Zaloguj</button>
+            </form>
+        </section>
 
     )
 }
