33b3c86efad4a04c26f1d03f02341de116fa3b09
Daerjon <01151573@pw.edu.pl>
2022-04-11 09:22:44 +0200

    bugfixy do modeli potrzebne przy wyświetlaniu obiektów i organizacji kodu

--- src/index.js
+++ src/index.js
@@ -6,25 +6,6 @@ import App from './components/App';
 import store from './app/store'
 import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals';
-import {AdminLayout} from "./components/admin/layout/AdminLayout";
-import {DoctorLayout} from "./components/doctor/layout/DoctorLayout";
-import {DoctorDashboard} from "./components/doctor/DoctorDashboard";
-import {DoctorAvailability} from "./components/doctor/DoctorAvalibility";
-import {DoctorPlanned} from "./components/doctor/DoctorPlanned";
-import {DoctorUnconfirmed} from "./components/doctor/DoctorUnconfirmed";
-import {DoctorHistory} from "./components/doctor/DoctorHistory";
-import {DoctorLoginPage} from "./components/doctor/DoctorLoginPage";
-import {PatientLayout} from "./components/patient/layout/PatientLayout";
-import {PatientDashboard} from "./components/patient/PatientDashboard";
-import {PatientCertifications} from "./components/patient/PatientCertifications";
-import {PatientHistory} from "./components/patient/PatientHistory";
-import {PatientPlanned} from "./components/patient/PatientPlanned";
-import {PatientSignup} from "./components/patient/PatientSignup";
-import {PatientLoginPage} from "./components/patient/PatientLoginPage";
-import {AdminPatientList} from "./components/admin/AdminPatientList";
-import {AdminDoctorList} from "./components/admin/AdminDoctorList";
-import Home from "./components/Home";
-import {AdminLoginPage} from "./components/admin/AdminLoginPage";
 
 ReactDOM.render(
   <Provider store={store}>
