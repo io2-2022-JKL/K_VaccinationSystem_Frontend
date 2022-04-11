92ac7e4a217da7ba1ac6ee23ce45b2127ed0628f
A-Gajda <ollagajda@gmail.com>
2022-04-11 03:38:57 +0200

    Zmiana strony zaplanowanych wizyt pacjenta

--- src/components/patient/PatientPlanned.js
+++ src/components/patient/PatientPlanned.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
+import DataTable from "../DataTable";
+
 export default function PatientPlanned() {
+
+    const [visits, setVisits] = useState([
+        {
+            Data: "12.04.2022",
+            Godzina: "13:10",
+            Miejsce: "Centrum Szczepień na Koszykowej",
+            Szczepionka: "Covid"
+        },
+        {
+            Data: "15.04.2022",
+            Godzina: "12:00",
+            Miejsce: "Centrum 8",
+            Szczepionka: "losowa"
+        }
+    ])
+
     return (
         <>
             <h2> Zaplanowane wizyty pacjenta </h2>
-
+            <DataTable list={visits}/>
         </>
-
-
     )
 }
