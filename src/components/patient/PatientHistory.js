cda8f4c8f95b0f852bd5eac803d7c84c38f6d79a
witek0509x <witek.droch@gmail.com>
2022-04-19 19:27:41 +0200

    patient history UI update

--- src/components/patient/PatientHistory.js
+++ src/components/patient/PatientHistory.js
@@ -1,43 +1,66 @@
-import { useState } from "react";
-import DataTable from "../tableComponent/DataTable";
+import React, {useState} from 'react';
+import '../../styles/patient/patient.css';
+import Patient from '../../models/Patient'
+import '../../models/User';
+import MDBox from "../MDBox";
+import DashboardLayout from "../../examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "../../examples/Navbars/DashboardNavbar";
+import Grid from "@mui/material/Grid";
+import Header from "../../layouts/profile/components/Header";
+import Divider from "@mui/material/Divider";
+import ProfileInfoCard from "../../examples/Cards/InfoCards/ProfileInfoCard";
+import Footer from "../../examples/Footer";
+import DataTable from "../../examples/Tables/DataTable";
 
-export default function PatientHistory() {
+export default function PatientDashboard() {
 
     const [history, setHistory] = useState([
         {
-            Data: "10.02.2022",
-            Szczepionka: "covid",
-            Odbyto: "tak"
+            Date: "10.02.2022",
+            Vaccine: "covid",
+            Completed: "tak"
         },
         {
-            Data: "2.02.2002",
-            Szczepionka: "przeziębienie",
-            Odbyto: "tak"
+            Date: "2.02.2002",
+            Vaccine: "przeziębienie",
+            Completed: "tak"
         },
         {
-            Data: "1.02.2022",
-            Szczepionka: "przeziębienie",
-            Odbyto: "nie"
-        },
-        {
-            Data: "21.01.2022",
-            Szczepionka: "świńska grypa",
-            Odbyto: "tak"
-        },
-        {
-            Data: "1.01.2022",
-            Szczepionka: "noworoczna choroba wysokoprocentowa",
-            Odbyto: "tak"
+            Date: "1.02.2022",
+            Vaccine: "przeziębienie",
+            Completed: "nie"
         }
     ])
+
+    const [patient, setPatient] = useState({
+        firstName: "Andrew",
+        lastName: "Bagpipe",
+
+    })
+
+    const tableColumns = [
+        {Header: "Data", accessor: "Date", width: "25%"},
+        {Header: "Szczepionka", accessor: "Vaccine", width: "50%"},
+        {Header: "Odbyto", accessor: "Completed", width: "25%"},
+    ]
+
+
+
     return (
-        <>
-            <div className="namePageSeparator">
-                <h2>
-                    Historia Szczepień
-                </h2>
-                <DataTable list={history}/>
-            </div>
-        </>
+        <DashboardLayout>
+            <DashboardNavbar/>
+            <MDBox mb={10}/>
+            <Header name={patient.firstName + " " + patient.lastName} position={"Pacjent"}>
+                <MDBox mt={5} mb={3}>
+                    <DataTable table={{columns: tableColumns, rows: history}}/>
+                </MDBox>
+            </Header>
+            <Footer/>
+        </DashboardLayout>
+
+
     )
 }
+
+
+
