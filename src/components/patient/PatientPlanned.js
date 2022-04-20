ea5f496688b240aa5e2f0b015967bc351c268af7
witek0509x <witek.droch@gmail.com>
2022-04-20 12:42:47 +0200

    planned UI update

--- src/components/patient/PatientPlanned.js
+++ src/components/patient/PatientPlanned.js
@@ -1,27 +1,59 @@
-import { useState } from "react";
-import DataTable from "../DataTable";
+import React, {useState} from 'react';
+import '../../styles/patient/patient.css';
+import Patient from '../../models/Patient'
+import '../../models/User';
+import MDBox from "../MDBox";
+import DashboardLayout from "../../examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "../../examples/Navbars/DashboardNavbar";
+import Grid from "@mui/material/Grid";
+import Header from "../../layouts/profile/components/Header";
+import Divider from "@mui/material/Divider";
+import ProfileInfoCard from "../../examples/Cards/InfoCards/ProfileInfoCard";
+import Footer from "../../examples/Footer";
+import DataTable from "../../examples/Tables/DataTable";
 
-export default function PatientPlanned() {
+export default function PatientDashboard() {
 
     const [visits, setVisits] = useState([
         {
-            Data: "12.04.2022",
-            Godzina: "13:10",
-            Miejsce: "Centrum Szczepień na Koszykowej",
-            Szczepionka: "Covid"
+            Date: "12.04.2022",
+            Hour: "13:10",
+            Place: "Centrum Szczepień na Koszykowej",
+            Vaccine: "Covid"
         },
         {
-            Data: "15.04.2022",
-            Godzina: "12:00",
-            Miejsce: "Centrum 8",
-            Szczepionka: "losowa"
+            Date: "15.04.2022",
+            Hour: "12:00",
+            Place: "Centrum 8",
+            Vaccine: "losowa"
         }
     ])
 
+    const [patient, setPatient] = useState({
+        firstName: "Andrew",
+        lastName: "Bagpipe",
+
+    })
+
+    const tableColumns = [
+        {Header: "Data", accessor: "Date", width: "25%"},
+        {Header: "Godzina", accessor: "Hour", width: "25%"},
+        {Header: "Miejsce", accessor: "Place", width: "25%"},
+        {Header: "Szczepionka", accessor: "Vaccine", width: "25%"},
+    ]
+
+
+
     return (
-        <div className='pageNameSeparator'>
-            <h2> Zaplanowane wizyty pacjenta </h2>
-            <DataTable list={visits}/>
-        </div>
+        <DashboardLayout>
+            <DashboardNavbar/>
+            <MDBox mb={10}/>
+            <Header name={patient.firstName + " " + patient.lastName} position={"Pacjent"}>
+                <MDBox mt={5} mb={3}>
+                    <DataTable table={{columns: tableColumns, rows: visits}}/>
+                </MDBox>
+            </Header>
+            <Footer/>
+        </DashboardLayout>
     )
 }
