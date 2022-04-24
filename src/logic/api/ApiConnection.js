dbe4af5bca86a83aac0ecfdc256fd9d4966951f8
witek0509x <witek.droch@gmail.com>
2022-04-24 18:52:13 +0200

    rebase on basic-styles next try

--- src/logic/api/ApiConnection.js
+++ src/logic/api/ApiConnection.js
@@ -4,18 +4,17 @@ import useLogin from "../useLogin";
 export default (path) => {
     const {GetToken} = useLogin();
     const token = GetToken(path);
-    //const url = "https://systemszczepien.azurewebsites.net/"
-    const url = `https://vaccinationsystemapi.azurewebsites.net`
+
     if(token === null)
     {
         return axios.create({
-            baseURL: url,
+            baseURL: `https://vaccinationsystemapi.azurewebsites.net`,
         });
     }
     else
     {
         return axios.create({
-            baseURL: url,
+            baseURL: `https://vaccinationsystemapi.azurewebsites.net`,
             authorization: "Bearer " + token,
         });
     }
