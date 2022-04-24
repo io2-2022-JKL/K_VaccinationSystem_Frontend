614e3671fa058a1d421b90b62a08c1d39abb7022
Daerjon <01151573@pw.edu.pl>
2022-04-24 16:35:47 +0200

    przygotowanie pod pisanie testów

--- src/tests/patient/PatientDashboard.test.js
+++ src/tests/patient/PatientDashboard.test.js
@@ -1,34 +1,41 @@
+import user  from '@testing-library/user-event';
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "assets/theme";
 import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
+import { render, screen, waitFor, unmountComponentAtNode } from "react-dom";
+import { Router, BrowserRouter, MemoryRouter } from 'react-router-dom';
 import { act } from "react-dom/test-utils";
+import { MaterialUIControllerProvider } from "context";
+import "babel-polyfill";
 
 import PatientDashboard from '../../components/patient/PatientDashboard';
 
-let container = null;
-beforeEach(() => {
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useLocation: () => ({
+    pathname: "localhost:3000/patient/"
+  })
+}));
 
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-it("renders page", () => {
-  act(() => {
-    render(<PatientDashboard />, container);
+describe("PatientDashboardTesting", () =>{
+  let container = null;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+    render(
+      <MemoryRouter>
+        <MaterialUIControllerProvider>
+          <ThemeProvider theme={theme}>
+            <PatientDashboard/>
+          </ThemeProvider>
+        </MaterialUIControllerProvider>
+      </MemoryRouter>, 
+      container);
   });
-  expect(container.textContent).toContain("Strona");
 
+  it("renders patient data", () => {
+    act(() => {});
+    expect(container.textContent).toContain("P");
+  }
+  );
 });
-
-it("renders patient data", () => {
-  act(() => {
-    render(<PatientDashboard/>, container);
-  });
-  expect(container.textContent).toContain("00000000001");
-}
-);
