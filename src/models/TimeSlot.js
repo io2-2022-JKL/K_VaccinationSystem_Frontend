7d36f4bf2f7730434084b9300d6f568f29d90af6
A-Gajda <ollagajda@gmail.com>
2022-05-21 18:18:15 +0200

    klasa TimeSlot

--- src/models/TimeSlot.js
+++ src/models/TimeSlot.js
@@ -1,43 +1,63 @@
-class TimeSlot
+import Doctor from "./Doctor";
+import VaccintationCenter from "./VaccinationCenter";
+
+export default class TimeSlot
 {
+    _id;
     _from;
     _to;
-    _doctor;
     _isFree;
-    _active;
+    _vaccinationCenter;
+    _doctor;
 
     constructor(timeSlot)
     {
+        this._id = timeSlot.id;
         this._from = timeSlot.from;
         this._to = timeSlot.to;
-        this._doctor = timeSlot.doctor;
         this._isFree = timeSlot.isFree;
-        this._active = timeSlot.active;
+        this._vaccinationCenter = new VaccintationCenter(timeSlot);
+        this._doctor = new Doctor(timeSlot);
     }
+
+    get getId() { return this._id; }
+    set setId(id) { this._id = id; }
 
     get getFrom() { return this._from; }
     set setFrom(from) { this._from = from; }
 
     get getTo() { return this._to; }
     set setTo(to) { this._to = to; }
-    
-    get getdoctor() { return this._doctor; }
-    set setdoctor(doctor) { this._doctor = doctor; }
 
     get getIsFree() { return this._isFree; }
     set setIsFree(isFree) { this._isFree = isFree; }
+    
+    get getDoctor() { return this._doctor; }
+    set setDoctor(doctor) { this._doctor = new Doctor(doctor); }
 
-    get getActive() { return this._active; }
-    set setActive(active) { this._active = active; }
-}
+    get getVaccinationCenter() { return this._vaccinationCenter; }
+    set setVaccinationCenter(center) { this._vaccinationCenter = new VaccintationCenter(center); }
 
-function createTimeSlot(from,to,doctor,isFree,active)
-{
-    timeSlot = new Object();
-    timeSlot.from = from;
-    timeSlot.to = to;
-    timeSlot.doctor = doctor;
-    timeSlot.isFree = isFree;
-    timeSlot.active = active;
-    return new TimeSlot(timeSlot);
+    toTableData() {
+        const NA = "NA";
+        let result = [];
+
+        this._id 
+        ? result = [...result, {timeSlotId: this._id}] 
+        : result = [...result, {timeSlotId: NA}];
+
+        this._from 
+        ? result = [...result, {from: this._from}] 
+        : result = [...result, {from: NA}];
+
+        this._to 
+        ? result = [...result, {to: this._to}] 
+        : result = [...result, {to: NA}];
+        
+        this._isFree 
+        ? result = [...result, {isFree: this._isFree}] 
+        : result = [...result, {isFree: NA}];
+
+        result = [...result, ...this._doctor.toTableData(), ...this._vaccinationCenter.toTableData()];
+    }
 }
