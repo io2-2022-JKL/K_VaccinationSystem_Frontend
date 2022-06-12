88c43f8ebfbf5a4e294b138fc5de805abc373586
Daerjon <01151573@pw.edu.pl>
2022-06-12 09:41:03 +0200

    kończenie admina

--- src/components/admin/AdminVaccinationCenterModals.js
+++ src/components/admin/AdminVaccinationCenterModals.js
@@ -35,41 +35,12 @@ export function AdminVaccinationCenterInfoModal(props) {
                     <div>Piątek: {props.data.openingHoursDays[4].from} - {props.data.openingHoursDays[4].to}</div>
                     <div>Sobota: {props.data.openingHoursDays[5].from} - {props.data.openingHoursDays[5].to}</div>
                     <div>Niedziela: {props.data.openingHoursDays[6].from} - {props.data.openingHoursDays[6].to}</div>
-                </Typography>
-            </Box>
-        </Modal>
-        </>
-    )
-}
-
-export function AdminDoctorInfoModal(props) {
-
-    const [open, setOpen] = useState(false);
-
-    const handleClose = () => setOpen(false);
-
-    const style = modalStyle()
-
-    return (
-        <>
-        <Button onClick={() => setOpen(true)}>Szczegóły</Button>
-        <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-        >
-            <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h2" component="h2">
-                    Informacje o doktorze
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }} variant="subtitle2" component="subtitle2">
-                    <div>Imię: {props.data.firstName} {props.data.lastName}</div>
-                    <div>PESEL: {props.data.pesel}</div>
-                    <div>Mail: {props.data.mail}</div>
-                    <div>Nr telefonu: {props.data.phoneNumber}</div>
-                    <div>Centrum szczepień: {props.data.name},{props.data.city},{props.data.street}</div>
-                    <div>Data urodzenia: {props.data.dateOfBirth}</div>
+                    <div>Szczepionki:</div>
+                    {props.data.vaccines.map((record) => (
+                        <div key={record.id}>
+                            {record.name}
+                        </div>
+                    ))}
                 </Typography>
             </Box>
         </Modal>
