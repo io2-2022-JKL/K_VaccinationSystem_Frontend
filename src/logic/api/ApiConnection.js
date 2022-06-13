56fa6738523bda82ff82b07ab749f83c9635acce
witek0509x <witek.droch@gmail.com>
2022-06-13 13:55:19 +0200

    set l backend

--- src/logic/api/ApiConnection.js
+++ src/logic/api/ApiConnection.js
@@ -4,16 +4,16 @@ import useLogin from "../useLogin";
 export default (path) => {
     const {GetToken} = useLogin();
     const token = GetToken(path);
-const backends = {
-        "k": `https://vaccinationsystemapi.azurewebsites.net`,
-        "j": "https://systemszczepien.azurewebsites.net/",
-        "l": "https://vaccinationsystemteaml.azurewebsites.net"
-    }
-    // let id = localStorage.getItem("backend");
-    // if(id === null)
-    //     localStorage.setItem("backend", "");
-    // const url = backends[localStorage.getItem("backend")];
-    const url = "https://systemszczepien.azurewebsites.net/";
+// const backends = {
+//         "k": `https://vaccinationsystemapi.azurewebsites.net`,
+//         "j": "https://systemszczepien.azurewebsites.net/",
+//         "l": "https://vaccinationsystemteaml.azurewebsites.net"
+//     }
+//     let id = localStorage.getItem("backend");
+//     if(id === null)
+//         localStorage.setItem("backend", "");
+//     const url = backends[localStorage.getItem("backend")];
+    const url = "https://vaccinationsystemteaml.azurewebsites.net";
     if(token === null)
     {
         return axios.create({
