d8dbab7fe7c6a184007c809011c7c8c07fc300ed
Daerjon <01151573@pw.edu.pl>
2022-06-13 10:51:35 +0200

    logi i zmiana bazy

--- src/logic/api/ApiConnection.js
+++ src/logic/api/ApiConnection.js
@@ -4,9 +4,9 @@ import useLogin from "../useLogin";
 export default (path) => {
     const {GetToken} = useLogin();
     const token = GetToken(path);
-    const url = "https://systemszczepien.azurewebsites.net/" //zespół j
+    //const url = "https://systemszczepien.azurewebsites.net/" //zespół j
     //const url = "https://vaccinationsystemteaml.azurewebsites.net" //zespół l
-    //const url = `https://vaccinationsystemapi.azurewebsites.net` //nasz
+    const url = `https://vaccinationsystemapi.azurewebsites.net` //nasz
     if(token === null)
     {
         return axios.create({
